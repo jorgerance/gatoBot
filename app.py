731ce6eeae0ad5b96eb09acebddbba23f635985a
root <gato@gatobot.com>
2017-11-08 19:07:10 +0000

    fixes + dynamic server ip in index.html

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
 import motors
 import RPi.GPIO as GPIO
+import socket
+
+# Get server ip
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+server_ip = s.getsockname()[0]
+s.close()
 
 GPIO.setmode(GPIO.BOARD) #set up GPIO
 
 app = Flask(__name__) #set up flask server
 
-#when the root IP is selected, return index.html page
 @app.route('/')
 def index():
 
-	return render_template('index.html')
+	return render_template('index.html', server_ip=server_ip)
 
-#recieve which pin to change from the button press on index.html
-#each button returns a number that triggers a command in this function
-#
-#Uses methods from motors.py to send commands to the GPIO to operate the motors
 @app.route('/<changepin>', methods=['POST'])
 def reroute(changepin):
 
